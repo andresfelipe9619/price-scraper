c3c17a37b9fe7f59fa985e8e2172059dd9dff214
Andrés Suárez <andresfelipe9619@gmail.com>
2025-02-26 08:37:40 -0500

    ♻️ Refactor Exito scraper to follow the 2025 page format

--- scraper/strategies/Exito.js
+++ scraper/strategies/Exito.js
@@ -1,123 +1,108 @@
-const fs = require('fs')
-const {
-  calculateDiscount,
-  getPrice,
-  getDiscount,
-  getRealPrice,
-  getImages,
-  getTitle,
-  bypassModal,
-  isElementVisible
-} = require('./interface')
-const { chunkArray } = require('../../utils')
+const fs = require('fs');
+const {chunkArray, normalizeDiscount, normalizePrice} = require('../../utils');
 
-const BASE_URL = 'https://www.exito.com'
-const PATH = '/mercado/lacteos-huevos-y-refrigerados'
+const BASE_URL = 'https://www.exito.com';
+const PATH = '/mercado/lacteos-huevos-y-refrigerados';
+const MAX_PAGES = 4;
 
-const Selectors = {
-  Content: '.vtex-product-summary-2-x-container',
-  City: '.MuiInput-input.exito-autocomplete-2',
-  Detail: '.exito-product-details-3-x-contentOptions',
-  LoadMore: '.vtex-search-result-3-x-buttonShowMore button div'
+async function scraper(browser) {
+  const page = await browser.newPage();
+  const url = BASE_URL + PATH;
+
+  console.log(`Navigating to: ${url}`);
+  await page.goto(url);
+
+  console.log(`Waiting for main page to load...`);
+  await page.waitForSelector('[class^="productCard_"]');
+  console.log(`Main page loaded!`);
+
+  let allProducts = [];
+  let pageIndex = 0;
+
+  do {
+    console.log(`Scraping products from page #${pageIndex + 1}...`);
+    const products = await getProductsFromPage(page, pageIndex);
+
+    console.log(`Adding ${products.length} products to the list...`);
+    allProducts = allProducts.concat(products);
+    if (pageIndex + 1 >= MAX_PAGES) {
+      console.log(`Reached the limit of ${MAX_PAGES} pages. Stopping.`);
+      break;
+    }
+
+    const nextPage = await isNextPageAvailable(page);
+    if (nextPage) {
+      console.log(`Navigating to next page (#${pageIndex + 2})...`);
+      await nextPage.click();
+      await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+    } else {
+      console.log('No more pages to scrape.');
+      break;
+    }
+
+    pageIndex++;
+  } while (true);
+
+  console.log(`Total products scraped: ${allProducts.length}`);
+
+  const object2write = {data: allProducts};
+  const data = JSON.stringify(object2write, null, 2);
+  await fs.promises.writeFile('exito-results.json', data);
+  console.log(`Data saved to exito-results.json`);
+
+  await page.close();
 }
 
-const ProductSelectors = {
-  Title: 'span.vtex-store-components-3-x-productBrand',
-  Discount: `div.exito-vtex-components-4-x-badgeDiscount`,
-  Images: 'img.exito-product-details-3-x-productImageTag--main',
-  Price: `${Selectors.Detail} div.exito-vtex-components-4-x-valuePLPAllied span`,
-  RealPrice: `${Selectors.Detail} div.vtex-flex-layout-0-x-flexRowContent--summaryPercentage span`,
-  PriceFallback: 'div.exito-vtex-components-4-x-selling-price span'
+async function getProductsFromPage(page, index = 0) {
+  let products = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('article[class^="productCard_"]')).map(product => {
+      const nameElement = product.querySelector('.styles_name__qQJiK');
+      const name = nameElement ? nameElement.innerText.trim() : null;
+
+      const imageElement = product.querySelector('img[alt="Imagen del producto"]');
+      const image = imageElement ? imageElement.src : null;
+
+      const priceElement = product.querySelector('[data-fs-container-price-otros]');
+      const price = priceElement ? priceElement.innerText.trim() : null;
+
+      const discountElement = product.querySelector('[class^="priceSection_container-promotion"]');
+      const discount = discountElement ? discountElement.innerText.trim() : null;
+
+      return {name, image, price, discount};
+    });
+  });
+
+  // products = products.map(({name, image, price, discountText}) => {
+  //   // Normalize the base and real prices
+  //   const finalPrice = normalizePrice(price);
+  //   const {percentage, amount} = normalizeDiscount(discountText);
+  //
+  //   // Calculate real price from discount amount or percentage
+  //   let realPrice = amount || (percentage ? finalPrice / (1 - percentage / 100) : finalPrice);
+  //
+  //   // Handle edge cases where realPrice might be invalid
+  //   realPrice = realPrice && realPrice > 0 ? realPrice : finalPrice;
+  //
+  //   // Calculate discount percentage, if not directly provided
+  //   const discountPercentage = percentage || ((realPrice - finalPrice) / realPrice * 100).toFixed(2);
+  //
+  //   return {
+  //     name,
+  //     image,
+  //     price: finalPrice,
+  //     realPrice,
+  //     discount: discountPercentage ? `${discountPercentage}%` : null
+  //   };
+  // });
+
+  console.log(`Found ${products.length} products on page #${index + 1}`);
+  return products;
 }
 
-async function scraper (browser) {
-  const page = await browser.newPage()
-  const url = BASE_URL + PATH
-
-  console.log(`Navigating to ${url}...`)
-  await page.goto(url)
-
-  await bypassModal(page, { Selectors })
-  console.log(`Waiting Main Page...`)
-
-  await page.click('.shippingaddress-confirmar')
-  await page.waitForSelector(Selectors.Content)
-
-  console.log(`Page Loaded...`)
-
-  let loadMoreVisible = await isElementVisible(page, Selectors.LoadMore)
-  while (loadMoreVisible) {
-    await page.click(Selectors.LoadMore).catch(error => console.error(error))
-    loadMoreVisible = await isElementVisible(page, Selectors.LoadMore)
-  }
-
-  let uris = await page.$$eval(Selectors.Content, options =>
-    options.map(option => {
-      if (option.firstChild) {
-        let children = option.firstChild
-        let link = children.getAttribute('href')
-        return link
-      }
-      return null
-    })
-  )
-  console.log(`uris`, uris)
-  // Loop through each of those links, open a new page instance and get the relevant data from them
-  const pagePromise = async link => {
-    const dataObj = {}
-    const newPage = await browser.newPage()
-    await newPage.goto(link)
-    dataObj['link'] = link
-
-    console.log(`Loading Tab ${link}...`)
-    await newPage.waitForSelector(ProductSelectors.Title)
-    console.log(`Tab Loaded ...`)
-
-    dataObj['title'] = await getTitle({ ProductSelectors })
-
-    await newPage.waitForSelector(Selectors.Detail)
-
-    const [
-      price = 0,
-      discount = null,
-      realPrice = 0,
-      images = []
-    ] = await Promise.all(
-      [getPrice, getDiscount, getRealPrice, getImages].map(fn =>
-        fn(newPage, { Selectors, ProductSelectors })
-      )
-    )
-
-    const calculatedDiscount = calculateDiscount(price, realPrice)
-
-    dataObj['price'] = price
-    dataObj['date'] = new Date()
-    dataObj['discount'] = discount || calculatedDiscount
-    dataObj['realPrice'] = realPrice
-    dataObj['images'] = images
-
-    await newPage.close()
-    return dataObj
-  }
-
-  const chunks = chunkArray(uris, 4)
-  console.log('chunks', chunks)
-
-  let result = await chunks.reduce(async (acc, chunk) => {
-    let accumulated = await acc
-    const chunkResult = await Promise.all(
-      chunk.map(uri => pagePromise(BASE_URL + uri))
-    )
-    accumulated.push(chunkResult)
-    return accumulated
-  }, Promise.resolve([]))
-  console.log(`result`, result)
-  result = result.flatMap(chunk => chunk)
-
-  const object2write = { data: result }
-  const data = JSON.stringify(object2write, null, 2)
-  await fs.promises.writeFile('exito-results.json', data)
-  console.log(`That's All Folks!!!`)
+async function isNextPageAvailable(page) {
+  const nextPageButton = await page.$('[class^="Pagination_nextPreviousLink__"]');
+  console.log(nextPageButton ? 'Next page button found!' : 'Next page button NOT found.');
+  return nextPageButton;
 }
 
-module.exports = scraper
+module.exports = scraper;
