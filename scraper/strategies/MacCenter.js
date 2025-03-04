5ecb1aa221cb358d0982717b6d26ac37f0ab7c56
Andrés Suárez <andresfelipe9619@gmail.com>
2025-03-04 11:16:51 -0500

    ✨ Add MacCenter and Tigo scrapers

---
+++ scraper/strategies/MacCenter.js
@@ -0,0 +1,30 @@
+const BaseScraper = require("../interfaces/BaseScraper");
+const {join} = require("path");
+
+class MacCenterScraper extends BaseScraper {
+  constructor(browser) {
+    const config = {
+      maxPages: 20,
+      baseUrl: "https://mac-center.com",
+      outputDir: join(__dirname, "../../extracted-data/mac-center"),
+      categories: {
+        iphone: "/search?sort=price%7Edesc&q=iphone",
+        "computadores-apple": "/search?sort=price%7Edesc&q=macbook",
+      },
+      nextPageText: 'página siguiente',
+      selectors: {
+        productCard: '.search-product-card',
+        title: ".card-head",
+        price: ".price__container_carousel",
+        specialPrice: ".price-old-class-1",
+        discount: '.price-segment-discount-1',
+        image: 'img.price__container_carousel',
+        link: 'a.full-unstyled-link',
+        nextPage: 'a',
+      },
+    };
+    super(browser, config);
+  }
+}
+
+module.exports = MacCenterScraper;
---
+++ scraper/strategies/Tigo.js
@@ -0,0 +1,29 @@
+const BaseScraper = require("../interfaces/BaseScraper");
+const {join} = require("path");
+
+class TigoScraper extends BaseScraper {
+  constructor(browser) {
+    const config = {
+      maxPages: 20,
+      baseUrl: "https://compras.tigo.com.co",
+      outputDir: join(__dirname, "../../extracted-data/tigo"),
+      categories: {
+        iphone: "/celulares/apple?O=OrderByTopSaleDESC&PS=24",
+      },
+      nextPageText: null,
+      selectors: {
+        productCard: '.celulares---tigo-colombia',
+        title: ".containerVersionReact--contentTitle__productName",
+        price: ".containerVersionReact--contentPrice__price",
+        specialPrice: null,
+        discount: '.containerVersionReact--contentDiscount--highlightPrice',
+        image: 'a.item-image > img',
+        link: 'a.item-image',
+        nextPage: null,
+      },
+    };
+    super(browser, config);
+  }
+}
+
+module.exports = TigoScraper;
