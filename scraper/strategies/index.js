7698c3035711225a155615efce8741047bc93c0e
Andrés Suárez <andresfelipe9619@gmail.com>
2025-03-10 05:00:23 -0500

    🚧 Create the full list of desirable scrapers

--- scraper/strategies/index.js
+++ scraper/strategies/index.js
@@ -1,16 +1,34 @@
-const Alkosto = require('./Alkosto')
-const Exito = require('./Exito')
-const Falabella = require('./Falabella')
-const IShop = require('./IShop')
-const Jumbo = require('./Jumbo')
-const Makro = require('./Makro')
-const Ebay = require('./Ebay')
-const Claro = require('./Claro')
-const MacCenter = require('./MacCenter')
-const MercadoLibre = require('./MercadoLibre')
-const Movistar = require('./Movistar')
-const Tigo = require('./Tigo')
-const Olimpica = require('./Olimpica')
+const Alkosto = require('./Alkosto');
+const Exito = require('./Exito');
+const Falabella = require('./Falabella');
+const IShop = require('./IShop');
+const Jumbo = require('./Jumbo');
+const Makro = require('./Makro');
+const Ebay = require('./Ebay');
+const Claro = require('./Claro');
+const MacCenter = require('./MacCenter');
+const MercadoLibre = require('./MercadoLibre');
+const Movistar = require('./Movistar');
+const Tigo = require('./Tigo');
+const Olimpica = require('./Olimpica');
+
+// const Homecenter = require('./Homecenter');
+// const Zara = require('./Zara');
+// const Adidas = require('./Adidas');
+// const Nike = require('./Nike');
+// const ArturoCalle = require('./ArturoCalle');
+// const PepeGanga = require('./PepeGanga');
+// const LaRebaja = require('./LaRebaja');
+// const Farmatodo = require('./Farmatodo');
+// const CruzVerde = require('./CruzVerde');
+// const D1 = require('./D1');
+// const Ara = require('./Ara');
+// const JustoBueno = require('./JustoBueno');
+// const Carulla = require('./Carulla');
+// const Panamericana = require('./Panamericana');
+// const Koaj = require('./Koaj');
+// const SpringStep = require('./SpringStep');
+// const Chevignon = require('./Chevignon');
 
 /** @type {Map<string, import('../interfaces/BaseScraper')>} */
 const Scrapers = new Map([
@@ -26,7 +44,24 @@ const Scrapers = new Map([
   ['tigo', Tigo],
   ['ebay', Ebay],
   ['claro', Claro],
-  ['maccenter', MacCenter]
-])
+  ['maccenter', MacCenter],
+  // ['larebaja', LaRebaja],
+  // ['farmatodo', Farmatodo],
+  // ['cruzverde', CruzVerde],
+  // ['homecenter', Homecenter],
+  // ['d1', D1],
+  // ['carulla', Carulla],
+  // ['ara', Ara],
+  // ['justobueno', JustoBueno],
+  // ['pepeganga', PepeGanga],
+  // ['panamericana', Panamericana],
+  // ['arturocalle', ArturoCalle],
+  // ['nike', Nike],
+  // ['adidas', Adidas],
+  // ['koaj', Koaj],
+  // ['zara', Zara],
+  // ['springstep', SpringStep],
+  // ['chevignon', Chevignon],
+]);
 
-module.exports = Scrapers
+module.exports = Scrapers;
