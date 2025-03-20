dbae292401296b3dd2064333c72842dce2c8ebf0
Andrés Suárez <andresfelipe9619@gmail.com>
2025-03-20 16:17:24 -0500

    ✅ Update tests

--- __tests__/scraper.js
+++ __tests__/scraper.js
@@ -1,24 +1,20 @@
-const runScraper = require('../scraper')
+const runScraper = require("../scraper");
 
 async function init() {
-  await runScraper('alkosto')
-  // await runScraper('exito')
-  // await runScraper('falabella')
-  // await runScraper('ishop')
-  // await runScraper('movistar')
-  // await runScraper('claro')
+  // await runScraper("alkosto"); // DEMO
+  // await runScraper("exito");
+  // await runScraper("falabella");
+  // await runScraper("ishop");
+  // await runScraper("movistar");
+  // await runScraper("claro");
   // await runScraper('maccenter') // DEMO
-  // await runScraper('tigo')
+  // await runScraper("tigo");
   // await runScraper('makro')
-  // await runScraper('jumbo')
-
-  // await runScraper('olimpica')
-  // await runScraper('mercadolibre')
-  // await runScraper('ebay')
-  // await runScraper('amazon')
-
-  // await Promise.all([runScraper('alkosto'), runScraper('maccenter')])
-
+  // await runScraper("mercadolibre");
+  // await runScraper("olimpica"); TO FIX
+  // await runScraper('jumbo'); TO FIX
+  // await Promise.all([runScraper("movistar"), runScraper("claro")]);
+  // await Promise.all([runScraper("jumbo"), runScraper("olimpica")]);
 }
 
-init()
+init();
--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,15 +1,21 @@
 // Import Jest (if needed in the environment)
-const { test, expect } = require('@jest/globals');
-const {normalizePrice, normalizePriceAndDiscount} = require("../utils");
+const { test, expect } = require("@jest/globals");
+const { normalizePrice, normalizePriceAndDiscount } = require("../utils");
 
 // Test normalizePrice function
-test('normalizePrice should convert string price to number', () => {
+test("normalizePrice should convert string price to number", () => {
   expect(normalizePrice("$ 11.600")).toBe(11600);
   expect(normalizePrice("$ 8.190.000")).toBe(8190000);
 });
 
 // Test normalizePriceAndDiscount function
-test('normalizePriceAndDiscount should convert string discount to number', () => {
-  expect(normalizePriceAndDiscount("-\n\n20\n\n%")).toEqual({ percentage: 20, amount: 0 });
-  expect(normalizePriceAndDiscount("-\n\n27\n\n%\n\n$ 15.500")).toEqual({ percentage: 27, amount: 15500 });
+test("normalizePriceAndDiscount should convert string discount to number", () => {
+  expect(normalizePriceAndDiscount("-\n\n20\n\n%")).toEqual({
+    percentage: 20,
+    amount: 0,
+  });
+  expect(normalizePriceAndDiscount("-\n\n27\n\n%\n\n$ 15.500")).toEqual({
+    percentage: 27,
+    amount: 15500,
+  });
 });
